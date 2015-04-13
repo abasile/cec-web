79c2af12f9d1921bf62f83303342fd8bf1efa012
Robbie Trencheny <me@robbiet.us>
2015-04-12 19:11:50 -0700

    General clean up, error catching where possible

--- cec-web.go
+++ cec-web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/jessevdk/go-flags"
 	"github.com/robbiet480/cec"
@@ -42,6 +43,23 @@ var volume_level = options.Audio.StartVolume
 var input_number int
 var is_muted = false
 
+func CheckForDevice() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var input string
+		if c.Params.ByName("device") != "" {
+			input = c.Params.ByName("device")
+		} else {
+			input = options.Audio.AudioDevice
+		}
+		if addr := cec.GetLogicalAddressByName(input); addr == -1 {
+			c.Fail(404, errors.New("That device ("+input+") does not exist!"))
+		} else {
+			c.Set("CECAddress", addr)
+			c.Next()
+		}
+	}
+}
+
 func main() {
 	if _, err := parser.Parse(); err != nil {
 		os.Exit(1)
@@ -54,36 +72,37 @@ func main() {
 			word = "an"
 		}
 		log.Println("You said you had " + word + " " + options.Audio.AudioDevice + " device but one cant be found!")
-		os.Exit(1)
+		// os.Exit(1)
 	}
 
 	r := gin.Default()
+	r.Use(gin.ErrorLogger())
 	r.GET("/config", config)
 	r.GET("/info", info)
 	r.GET("/input", input_status)
 	r.PUT("/input/:number", input_change)
-	r.GET("/power/:device", power_status)
-	r.PUT("/power/:device", power_on)
-	r.DELETE("/power/:device", power_off)
-	r.GET("/volume", vol_status)
-	r.PUT("/volume/up", vol_up)
-	r.PUT("/volume/down", vol_down)
-	r.PUT("/volume/mute", vol_mute)
-	r.GET("/volume/mute", vol_mute_status)
+	r.GET("/power/:device", CheckForDevice(), power_status)
+	r.PUT("/power/:device", CheckForDevice(), power_on)
+	r.DELETE("/power/:device", CheckForDevice(), power_off)
+	r.GET("/volume", CheckForDevice(), vol_status)
+	r.PUT("/volume/up", CheckForDevice(), vol_up)
+	r.PUT("/volume/down", CheckForDevice(), vol_down)
+	r.PUT("/volume/mute", CheckForDevice(), vol_mute)
+	r.GET("/volume/mute", CheckForDevice(), vol_mute_status)
 	r.PUT("/volume/reset", vol_reset)
-	r.PUT("/volume/step/:direction/:steps", vol_step)
-	r.PUT("/volume/set/:level", vol_set)
+	r.PUT("/volume/step/:direction/:steps", CheckForDevice(), vol_step)
+	r.PUT("/volume/set/:level", CheckForDevice(), vol_set)
 	r.PUT("/volume/force/:level", vol_force_set)
-	r.PUT("/key/:device/:key", key)
-	r.PUT("/multikey/:device/:key/:delay/:key2", multi_key)
+	r.PUT("/key/:device/:key", CheckForDevice(), key)
+	r.PUT("/multikey/:device/:key/:delay/:key2", CheckForDevice(), multi_key)
 	r.PUT("/channel/:device/:channel", change_channel)
 	r.POST("/transmit", transmit)
 
 	if options.Audio.ResetVolume != true {
 		// Let's reset the volume level to 0
 		log.Println("Resetting volume to 0")
+		addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
 		for i := 0; i < options.Audio.MaxVolume; i++ {
-			addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
 			log.Println("Sending VolumeDown")
 			cec.Key(addr, "VolumeDown")
 			volume_level = 0
@@ -111,25 +130,36 @@ func config(c *gin.Context) {
 }
 
 func info(c *gin.Context) {
-	c.JSON(200, cec.List())
+	list := cec.List()
+	if list != nil {
+		c.JSON(200, list)
+	} else {
+		c.String(500, "")
+	}
 }
 
 func power_on(c *gin.Context) {
-	addr := cec.GetLogicalAddressByName(c.Params.ByName("device"))
+	addr := c.MustGet("CECAddress").(int)
 
-	cec.PowerOn(addr)
-	c.String(200, "on")
+	if err := cec.PowerOn(addr); err != nil {
+		c.Fail(500, err)
+	} else {
+		c.String(200, "on")
+	}
 }
 
 func power_off(c *gin.Context) {
-	addr := cec.GetLogicalAddressByName(c.Params.ByName("device"))
+	addr := c.MustGet("CECAddress").(int)
 
-	cec.Standby(addr)
-	c.String(200, "off")
+	if err := cec.Standby(addr); err != nil {
+		c.Fail(500, err)
+	} else {
+		c.String(200, "off")
+	}
 }
 
 func power_status(c *gin.Context) {
-	addr := cec.GetLogicalAddressByName(c.Params.ByName("device"))
+	addr := c.MustGet("CECAddress").(int)
 
 	status := cec.GetDevicePowerStatus(addr)
 	if status == "on" {
@@ -154,7 +184,7 @@ func input_change(c *gin.Context) {
 }
 
 func change_channel(c *gin.Context) {
-	addr := cec.GetLogicalAddressByName(c.Params.ByName("device"))
+	addr := c.MustGet("CECAddress").(int)
 	channel := c.Params.ByName("channel")
 
 	for _, number := range channel {
@@ -169,22 +199,25 @@ func vol_step(c *gin.Context) {
 	steps_atoi, _ := strconv.Atoi(steps_str)
 	steps := int(steps_atoi)
 	direction := c.Params.ByName("direction")
+	addr := c.MustGet("CECAddress").(int)
+
+	if direction != "up" && direction != "down" {
+		c.Fail(400, errors.New("Invalid direction. Valid directions are up or down."))
+	}
 
 	for i := 0; i < steps; i++ {
 		if direction == "up" {
-			addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
 			cec.Key(addr, "VolumeUp")
 			volume_level = volume_level + steps
 		} else if direction == "down" {
-			addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
 			cec.Key(addr, "VolumeDown")
 			volume_level = volume_level - steps
-		} else {
-			c.String(400, "Invalid direction. Valid directions are up or down.")
 		}
 	}
 
-	c.String(204, "")
+	if c.LastError == nil {
+		c.String(204, "")
+	}
 }
 
 func vol_set(c *gin.Context) {
@@ -194,6 +227,8 @@ func vol_set(c *gin.Context) {
 
 	log.Println("Wanted_level is " + strconv.Itoa(wanted_level) + " and volume_level is " + strconv.Itoa(volume_level))
 
+	addr := c.MustGet("CECAddress").(int)
+
 	if wanted_level > options.Audio.MaxVolume {
 		c.String(400, "The maximum volume level is "+strconv.Itoa(options.Audio.MaxVolume))
 	} else if wanted_level > volume_level { // Requested level is greater then current volume level
@@ -201,7 +236,6 @@ func vol_set(c *gin.Context) {
 		var final_level = wanted_level - volume_level
 		log.Println("Final_level is " + strconv.Itoa(final_level))
 		for i := 0; i < final_level; i++ {
-			addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
 			cec.Key(addr, "VolumeUp")
 		}
 	} else if wanted_level < volume_level { // Requested level is less than current volume level
@@ -209,7 +243,6 @@ func vol_set(c *gin.Context) {
 		var final_level = volume_level - wanted_level
 		log.Println("Final_level is " + strconv.Itoa(final_level))
 		for i := 0; i < final_level; i++ {
-			addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
 			cec.Key(addr, "VolumeDown")
 		}
 	}
@@ -238,7 +271,7 @@ func vol_up(c *gin.Context) {
 	if volume_level == options.Audio.MaxVolume {
 		c.String(400, "Volume already at maximum")
 	} else {
-		addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
+		addr := c.MustGet("CECAddress").(int)
 		cec.Key(addr, "VolumeUp")
 		volume_level = volume_level + 1
 		c.String(204, "")
@@ -249,7 +282,7 @@ func vol_down(c *gin.Context) {
 	if volume_level == 0 {
 		c.String(400, "Volume is already at minimum")
 	} else {
-		addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
+		addr := c.MustGet("CECAddress").(int)
 		cec.Key(addr, "VolumeDown")
 		volume_level = volume_level - 1
 		c.String(204, "")
@@ -267,10 +300,9 @@ func vol_mute_status(c *gin.Context) {
 }
 
 func vol_reset(c *gin.Context) {
+	addr := c.MustGet("CECAddress").(int)
 	for i := 0; i < options.Audio.MaxVolume; i++ {
 		log.Println("Sending VolumeDown")
-		addr := cec.GetLogicalAddressByName(options.Audio.AudioDevice)
-
 		cec.Key(addr, "VolumeDown")
 	}
 	volume_level = 0
@@ -278,7 +310,7 @@ func vol_reset(c *gin.Context) {
 }
 
 func key(c *gin.Context) {
-	addr := cec.GetLogicalAddressByName(c.Params.ByName("device"))
+	addr := c.MustGet("CECAddress").(int)
 	key := c.Params.ByName("key")
 
 	cec.Key(addr, key)
@@ -286,7 +318,7 @@ func key(c *gin.Context) {
 }
 
 func multi_key(c *gin.Context) {
-	addr := cec.GetLogicalAddressByName(c.Params.ByName("device"))
+	addr := c.MustGet("CECAddress").(int)
 	key := c.Params.ByName("key")
 	key_two := c.Params.ByName("key2")
 	delay, _ := strconv.Atoi(c.Params.ByName("delay"))
